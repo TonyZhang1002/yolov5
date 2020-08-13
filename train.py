8ebdd345cd860652be64dbb322a49062582b62bc
TonyZhang1002 <z496722204a@hotmail.com>
2020-08-13 22:51:27 +0100

    new updates for FGSM

--- train.py
+++ train.py
@@ -217,7 +217,7 @@ def train(hyp):
     print('Image sizes %g train, %g test' % (imgsz, imgsz_test))
     print('Using %g dataloader workers' % dataloader.num_workers)
     print('Starting training for %g epochs...' % epochs)
-    # torch.autograd.set_detect_anomaly(True)
+    torch.autograd.set_detect_anomaly(True)
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         model.train()
 
@@ -265,6 +265,9 @@ def train(hyp):
             if not torch.isfinite(loss):
                 print('WARNING: non-finite loss, ending training ', loss_items)
                 return results
+
+            # Zero all existing gradients
+            model.zero_grad()
 
             # Backward
             if mixed_precision:
